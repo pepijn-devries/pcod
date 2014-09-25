f5fbbd3c1caa094fb635b74bd71f187bba37b078
= <=>
2014-09-25 13:43:25 +0100

    First check in of the file to write a config file

---
+++ R/config.R
@@ -0,0 +1,21 @@
+# config.R - to allow users to set all relevant parameters
+# in one location. This will be sourced first during the run
+# and parameters will be updated from these inputs
+
+# Management Unit
+
+# Population Size
+
+# Pup Survival Rate
+
+# Juvenile Survival Rate
+
+# Adult Survival Rate
+
+# Age at first birth
+
+# Fertility Rate
+
+# Vulnerable population
+
+# Residual Days of Disturbance
