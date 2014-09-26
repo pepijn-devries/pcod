305724be93fd3586323c5935243ee82bdcb8fdae
= <=>
2014-09-26 11:53:11 +0100

    first check in of the file to make the elicitation data into final rda files that are just in the package.

---
+++ R/makeElicitData.R
@@ -0,0 +1,7 @@
+# make the outputs from the elicitation into a proper .rda object
+load("../data/HS_mature_Days.robj"); matureDays <- temp
+load("../data/output_matureHS_290113.robj")
+load("../data/HSjuve_Days.robj"); juveDays <- temp              
+load("../data/HSdependent_Days.robj"); dependentDays <- temp    
+load("../data/output_juvenileHS_290113.robj")                   
+load("../data/output_dependentHS_290113.robj") 
