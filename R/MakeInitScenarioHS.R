988c4962478e2f1251cf9bae20300815839e4511
= <=>
2014-09-26 11:49:10 +0100

    removing the data loading from MakeInit, so that it can sit in data as an rda object

--- R/MakeInitScenarioHS.R
+++ R/MakeInitScenarioHS.R
@@ -1,14 +1,7 @@
 spec <- 'HS'
 propfemale <- 0.5
 
-# Load in the appropriate values from the results of the expert elicitation
-# link to text B
-load("../data/HS_mature_Days.robj"); matureDays <- temp
-load("../data/output_matureHS_290113.robj")
-load("../data/HSjuve_Days.robj"); juveDays <- temp              
-load("../data/HSdependent_Days.robj"); dependentDays <- temp    
-load("../data/output_juvenileHS_290113.robj")                   
-load("../data/output_dependentHS_290113.robj")                  
+                 
 # set threshold value of total population size for inclusion of demographic stochasticity
 threshold <- 500
 
